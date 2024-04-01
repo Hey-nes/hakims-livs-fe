cb03c54975b24a84143cd6e3085c703629be2cb5
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-04-01 10:44:10 +0200

    added basic front end, cleaned up project files and fixed some bugs that caused the build not to start

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <h5>Det här är footern</h5>
+        <ul>
+          <li>
+            <a href="">Länk 1</a>
+          </li>
+          <li>
+            <a href="">Länk 2</a>
+          </li>
+          <li>
+            <a href="">Länk 3</a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+const Navigation = () => {
+  return (
+    <div>
+      <aside className="aside">
+        <h3>Kategorier</h3>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="">Kategori 1</a>
+            </li>
+            <li>
+              <a href="">Kategori 2</a>
+            </li>
+            <li>
+              <a href="">Kategori 3</a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </div>
+  );
+};
+
+export default Navigation;
