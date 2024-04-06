c7997a9fbb8824009fd1da8cc9ac3297c16fe8b1
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2024-04-06 20:18:03 +0200

    removed redundant back end

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const cartRouter = require("./src/routes/cart.route");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api/intro/cart", cartRouter )
-module.exports = app;
--- backend/src/routes/cart.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {getCarts ,getOneItem, createNewItem} = require("../controllers/cartController");
-const router = express.Router();
-
-router.get("/", getCarts);
-router.get("/:id",getOneItem );
-router.post("/create",createNewItem );
-
-module.exports = router;
